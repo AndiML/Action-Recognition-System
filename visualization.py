ee2a82b74d721f41f5bab1028ae4b1a7e57c1953
Andreas Lutz <andreasl.95@web.de>
2024-06-28 14:26:25 +0200

    Integrated landmark extraction for pose face and hands serving as feature vector for action recognition

--- visualization.py
+++ visualization.py
@@ -1,42 +1,88 @@
-import mediapipe as mp
+import mediapipe
 
 class LandmarkDrawer:
-    def __init__(self):
-        # Initialize drawing specifications with fixed colors for each component
-        self.face_drawing_spec = mp.solutions.drawing_utils.DrawingSpec(
-            color=(255, 0, 0), thickness=1, circle_radius=1)  # Red for face
-        self.right_hand_drawing_spec = mp.solutions.drawing_utils.DrawingSpec(
-            color=(0, 255, 0), thickness=2, circle_radius=2)  # Green for right hand
-        self.left_hand_drawing_spec = mp.solutions.drawing_utils.DrawingSpec(
-            color=(0, 0, 255), thickness=2, circle_radius=2)  # Blue for left hand
-        self.pose_drawing_spec = mp.solutions.drawing_utils.DrawingSpec(
-            color=(255, 255, 0), thickness=3, circle_radius=3)  # Yellow for pose
+    """
+    A utility class for drawing landmarks on an image.
+    Utilizes MediaPipe's drawing utilities and predefined drawing specifications.
+    """
 
-        # Initialize MediaPipe drawing utils and holistic model
-        self.mp_drawing = mp.solutions.drawing_utils
-        self.mp_holistic = mp.solutions.holistic
+    def __init__(self):
+        """
+        Initializes the LandmarkDrawer with specific drawing specifications for different landmarks.
+        """
+        # Initialize drawing specifications with fixed colors for each component
+        self.face_drawing_spec = mediapipe.solutions.drawing_utils.DrawingSpec(
+            color=(255, 0, 0),
+            thickness=1, 
+            circle_radius=1
+        ) 
+        
+        self.right_hand_drawing_spec = mediapipe.solutions.drawing_utils.DrawingSpec(
+            color=(0, 255, 0),
+            thickness=2,
+            circle_radius=2
+        ) 
+        
+        self.left_hand_drawing_spec = mediapipe.solutions.drawing_utils.DrawingSpec(
+            color=(0, 0, 255), 
+            thickness=2,
+            circle_radius=2
+        ) 
+       
+        self.pose_drawing_spec = mediapipe.solutions.drawing_utils.DrawingSpec(
+            color=(255, 255, 0), 
+            thickness=2, 
+            circle_radius=2,
+        )  
+
+        # Initialize MediaPipe drawing utils
+        self.mp_drawing = mediapipe.solutions.drawing_utils
 
     def draw_landmarks(self, image, pose_detection_results):
+        """
+        Draws the landmarks on the given image based on the detection results.
+
+        Args:
+            image (numpy.ndarray): The image on which landmarks are to be drawn.
+            pose_detection_results (mediapipe.framework.formats.landmark_pb2.NormalizedLandmarkList): The detection results containing landmarks.
+        """
         # Draw face landmarks
         if pose_detection_results.face_landmarks:
             self.mp_drawing.draw_landmarks(
-                image, pose_detection_results.face_landmarks, self.mp_holistic.FACEMESH_TESSELATION,
-                self.face_drawing_spec, self.face_drawing_spec)
+                image, 
+                pose_detection_results.face_landmarks, 
+                mediapipe.solutions.holistic.FACEMESH_CONTOURS,
+                self.face_drawing_spec,
+                self.face_drawing_spec
+            )
         
         # Draw right hand landmarks
         if pose_detection_results.right_hand_landmarks:
             self.mp_drawing.draw_landmarks(
-                image, pose_detection_results.right_hand_landmarks, self.mp_holistic.HAND_CONNECTIONS,
-                self.right_hand_drawing_spec, self.right_hand_drawing_spec)
+                image, 
+                pose_detection_results.right_hand_landmarks, 
+                mediapipe.solutions.holistic.HAND_CONNECTIONS,
+                self.right_hand_drawing_spec,
+                self.right_hand_drawing_spec
+            )
 
         # Draw left hand landmarks
         if pose_detection_results.left_hand_landmarks:
             self.mp_drawing.draw_landmarks(
-                image, pose_detection_results.left_hand_landmarks, self.mp_holistic.HAND_CONNECTIONS,
-                self.left_hand_drawing_spec, self.left_hand_drawing_spec)
+                image,
+                pose_detection_results.left_hand_landmarks, 
+                mediapipe.solutions.holistic.HAND_CONNECTIONS,
+                self.left_hand_drawing_spec,
+                self.left_hand_drawing_spec
+            )
 
         # Draw pose landmarks
         if pose_detection_results.pose_landmarks:
             self.mp_drawing.draw_landmarks(
-                image, pose_detection_results.pose_landmarks, self.mp_holistic.POSE_CONNECTIONS,
-                self.pose_drawing_spec, self.pose_drawing_spec)
+                image, pose_detection_results.pose_landmarks,
+                mediapipe.solutions.holistic.POSE_CONNECTIONS,
+                self.pose_drawing_spec,
+                self.pose_drawing_spec
+            )
+    
+
